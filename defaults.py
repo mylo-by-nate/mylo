bc01af1523b5440eda3f8d14cf06af40e87fcc4c
Mylo <mylo.by.nate@gmail.com>
2021-10-13 16:13:32 -0500

    brain

---
+++ defaults.py
@@ -0,0 +1,23 @@
+
+nate = { #MB
+    "info": { #FOLD
+        "personal": { #CELL
+            "templatephrase": ["nate's NAME is ANSWER.", "well since you're so curious, nate's NAME is ANSWER.",
+                "I remember nate's NAME being ANSWER.", "I currently have nate's NAME saved as ANSWER."],
+            'templatekeys':['what is nates NAME', 'is nates NAME ANSWER', 'can you tell me nates NAME', 'do you remember nates NAME'],
+            "age": { #NEURON
+                'addkeys': ['how old is nate', 'what is nates age', 'how old is nathan'],
+                'ans': '20',
+                'fire': None,
+                'phrase': None,
+            },
+            "birthday": { #NEURON
+                'addkeys': ['what is nates birthday', 'when is nates birthday'],
+                'ans': 'August 16th, 2001',
+                'fire': None,
+                'phrase': None
+            },
+            
+        }
+    }
+}
